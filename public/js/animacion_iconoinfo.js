78cb26aa0b763b3872f266f48b90831e82972b00
mcorteslozano <corteslozanomario@gmail.com>
2020-04-24 16:55:21 +0200

    24/04/2020 CambiosIconos-NuevoFooter

--- public/js/animacion_iconoinfo.js
+++ public/js/animacion_iconoinfo.js
@@ -3,6 +3,7 @@ window.onload = function(){
     var icono_informacion = document.getElementById('icono_informacion');
     icono_informacion.addEventListener('click', function() {
         alert('¡Ops! Todavía no hay manual de uso de la aplicación disponible.');
+        window.open('/resources/ManualGestión.pdf');
     });
 
 }
