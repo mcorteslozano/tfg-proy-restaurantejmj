26a67bade973e81a2893c2c1d96a17645bfa05c6
mcorteslozano <corteslozanomario@gmail.com>
2020-05-05 15:26:00 +0200

    05/05/2020 Manual de Usuario

--- public/js/animacion_iconoinfo.js
+++ public/js/animacion_iconoinfo.js
@@ -2,7 +2,6 @@ window.onload = function(){
 
     var icono_informacion = document.getElementById('icono_informacion');
     icono_informacion.addEventListener('click', function() {
-        alert('¡Ops! Todavía no hay manual de uso de la aplicación disponible.');
         window.open('/resources/ManualDeUsuario.pdf');
     });
 
