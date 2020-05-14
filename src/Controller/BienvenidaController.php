d1ed064758b4fd4feb3da49d5f5c228d1a5b574a
mcorteslozano <corteslozanomario@gmail.com>
2020-05-14 19:25:07 +0200

    14/05/2020 Bienvenida:DistintosForm-Carta.js

--- src/Controller/BienvenidaController.php
+++ src/Controller/BienvenidaController.php
@@ -57,7 +57,7 @@ class BienvenidaController extends AbstractController
             $mensaje = (new \Swift_Message('Nuevo mensaje de un cliente: EVENTO'))
             ->setFrom($contacto['email'])
             ->setTo('restaurantejmj@gmail.com')
-            ->setBody('Se ha registrado un nuevo mensaje de un cliente a través de la página web www.restaurantejmj.es <br><br><b>Nombre del cliente:</b> '.$contacto['nombre']. '<br><b>Número de personas aprox.:</b>'.$contacto['personas'].'<br><b>Fecha de reserva:</b>'.$contacto['fecha']->format('Y-m-d').'<br><b>Correo electrónico:</b>'.$contacto['email'].'<br><b>Mensaje:</b> '.$contacto['mensaje']
+            ->setBody('Se ha registrado un nuevo mensaje de un cliente a través de la página web www.restaurantejmj.es <br><br><b>Nombre del cliente:</b> '.$contacto['nombre']. '<br><b>Número de personas aprox.:</b>'.$contacto['numpersonas'].'<br><b>Fecha de reserva:</b>'.$contacto['fecha']->format('Y-m-d').'<br><b>Correo electrónico:</b>'.$contacto['email'].'<br><b>Mensaje:</b> '.$contacto['mensaje']
             ,  'text/html');
 
             $mailer->send($mensaje);
